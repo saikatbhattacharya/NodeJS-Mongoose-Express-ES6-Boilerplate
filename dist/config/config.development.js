4e704166fd35b91f69dd1a75bb2b032431ab60a6
Saikat Bhattacharya <saikat.bhattacharya@nationstarmail.com>
2018-01-31 17:33:25 +0530

    first commit

--- dist/config/config.development.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-module.exports = {
-    "mongodb": "mongodb://billappadmin:admin123@ds163060.mlab.com:63060/billapp"
-};
--- dist/config/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var configFilePath = process.env.NODE_ENV ? './config.' + process.env.NODE_ENV + '.js' : './config.development.js';
-var config = require(configFilePath);
-
-exports.default = config;
